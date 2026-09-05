5fd74ef17278a2bff49eea28ba033c465d9b097d
James Patrick <257362047+jamespatrick29170@users.noreply.github.com>
2026-09-04 21:27:02 -0400

    added in vars and function to check name field and throw a visible error

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//placeholder
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const nameError = nameInput.nextElementSibling;
+
+if (form) {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log('name value:', nameInput.value);
+        if (nameInput.value.trim().length < 2) {
+            nameError.textContent = 'Name must be 2 characters or more'
+            nameError.style.display = 'block';
+        } else {
+            nameError.textContent = '';
+            nameError.style.display = 'none';
+        }
+    })
+}
